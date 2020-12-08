f3aa66918d51cb164400e683d15c1814f6088b88
Eyakhechine <74769270+Eyakhechine@users.noreply.github.com>
2020-12-08 20:36:53 +0100

    gestion produits

--- Controller/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('database.php');
-$id=$_GET['id'];
-$mysqli->query("delete from productss where id=$id");
-unlink("../assets/profile_images/".$id.".jpg");
-header('location:../views/index.php');
-?>
